cedd7f0adb98d5c51ca84fd4446a2f9657089c0c
bruce.wang <bruce.wang@zerologix.com>
2020-06-03 17:09:33 +1000

    remove duplicated code

--- application/themes/customised/components/homepageSections/header/header.js
+++ application/themes/customised/components/homepageSections/header/header.js
@@ -20,16 +20,4 @@ mobileBtn.on("click", function () {
   mobileNav.fadeIn(300);
 });
 
-// $(function () {
-//   // bind change event to select
-//   $("#language-switch").on("change", function () {
-//     var url = $(this).val(); // get selected value
-//     if (url) {
-//       // require a URL
-//       window.location =window.location.href+url; // redirect
-//     }
-//     return false;
-//   });
-// });
-
 listenAnchor();
