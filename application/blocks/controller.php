09898c894573440c856d1963033ea6b88bc81dcd
bruce.wang <bruce.wang@zerologix.com>
2020-06-09 18:15:23 +1000

    update mobile layout and ie support for tradingcup

--- application/blocks/controller.php
+++ application/blocks/controller.php
@@ -24,9 +24,11 @@ class Controller extends BlockController{
     protected $homePageID;
     protected $btTable = 'btNavigation';
     protected $btInterfaceWidth = 700;
+    public $cParentIDArray = array();
     protected $btInterfaceHeight = 525;
     protected $btCacheBlockRecord = true;
     protected $btCacheBlockOutput = true;
+    public $displayPages, $displayPagesCID, $displayPagesIncludeSelf, $displaySubPages, $displaySubPageLevels, $displaySubPageLevelsNum, $orderBy, $displayUnavailablePages;
     protected $btCacheBlockOutputOnPost = true;
     protected $btCacheBlockOutputForRegisteredUsers = false;
     protected $btCacheBlockOutputLifetime = 300;
@@ -35,6 +37,10 @@ class Controller extends BlockController{
     protected $btExportPageColumns = array('displayPagesCID');
     protected $includeParentItem;
 
+    /**
+     * Controller constructor.
+     * @param null $obj
+     */
     public function __construct($obj = null)
     {
         if (is_object($obj)) {
@@ -72,6 +78,112 @@ class Controller extends BlockController{
         return t("Creates a customised blocks.");
     }
 
+    public function getBlockTypeName()
+    {
+        return t("Customized-Block");
+    }
+
+    public function getContent()
+    {
+        /* our templates expect a variable not an object */
+        $con = array();
+        foreach ($this as $key => $value) {
+            $con[$key] = $value;
+        }
+
+        return $con;
+    }
+
+    /**
+     * @param $c
+     * @return array
+     */
+    public function getChildPages($c)
+    {
+
+        // a quickie
+        $db = Database::connection();
+        $r = $db->query(
+            "select cID from Pages where cParentID = ? order by cDisplayOrder asc",
+            array($c->getCollectionID()));
+        $pages = array();
+        while ($row = $r->fetchRow()) {
+            $pages[] = Page::getByID($row['cID'], 'ACTIVE');
+        }
+
+        return $pages;
+    }
+
+    /**
+     * @return int
+     */
+    public function getParentParentID()
+    {
+        // this has to be the stupidest name of a function I've ever created. sigh
+        $cParentID = Page::getCollectionParentIDFromChildID($this->cParentID);
+
+        return ($cParentID) ? $cParentID : 0;
+    }
+
+    /**
+     * @param $level
+     * @return mixed|null
+     */
+
+    public function getParentAtLevel($level)
+    {
+        // this function works in the following way
+        // we go from the current collection up to the top level. Then we find the parent Id at the particular level specified, and begin our
+        // autonav from that point
+
+        $this->populateParentIDArray($this->cID);
+
+        $idArray = array_reverse($this->cParentIDArray);
+        $this->cParentIDArray = array();
+        if ($level - count($idArray) == 0) {
+            // This means that the parent ID array is one less than the item
+            // we're trying to grab - so we return our CURRENT page as the item to get
+            // things under
+            return $this->cID;
+        }
+
+        if (isset($idArray[$level])) {
+            return $idArray[$level];
+        } else {
+            return null;
+        }
+    }
+
+    public function populateParentIDArray($cID)
+    {
+        // returns an array of collection IDs going from the top level to the current item
+        $cParentID = Page::getCollectionParentIDFromChildID($cID);
+        if (is_numeric($cParentID)) {
+            if (!in_array($cParentID, $this->cParentIDArray, true)) {
+                $this->cParentIDArray[] = $cParentID;
+            }
+            if ($cParentID > 0) {
+                $this->populateParentIDArray($cParentID);
+            }
+        }
+    }
+
+    protected function displayPage($tc)
+    {
+        $tcv = $tc->getVersionObject();
+        if ((!is_object($tcv)) || (!$tcv->isApprovedNow() && !$this->displayUnapproved)) {
+            return false;
+        }
+
+        if ($this->displayUnavailablePages == false) {
+            $tcp = new Permissions($tc);
+            if (!$tcp->canRead()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
 
 }
