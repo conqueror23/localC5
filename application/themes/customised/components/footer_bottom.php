560359bf0d4efd08ff7497bd4f8a5871fbf3f6f0
bruce.wang <bruce.wang@zerologix.com>
2020-06-10 14:15:37 +1000

    update hash code for getCoutnryList

--- application/themes/customised/components/footer_bottom.php
+++ application/themes/customised/components/footer_bottom.php
@@ -3,7 +3,7 @@
 </div>
 <script src="<?= $this->getThemePath()?>/components/homepageSections/subscribeForm/getCountryList-cc31a.js"></script>
 <script src="<?= $this->getThemePath() ?>/components/homepageSections/subscribeForm/submitSub.js"></script>
-<script src="<?= $this->getThemePath() ?>/components/homepageSections/subscribeForm/getCountryDropDownList.js"></script>
+<script src="<?= $this->getThemePath() ?>/components/homepageSections/subscribeForm/getCountryDropDownList-cc31a.js"></script>
 <!--<script src="--><?//= $this->getThemePath() ?><!--/components/homepageSections/highlight2019/assets/index.js"></script>-->
 <?php View::element('footer_required'); ?>
 
