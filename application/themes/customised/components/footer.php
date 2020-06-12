8874ea2a9db93ca20877989791faf81fc4a5d617
bruce.wang <bruce.wang@zerologix.com>
2020-06-12 15:23:40 +1000

    update translation process

--- application/themes/customised/components/footer.php
+++ application/themes/customised/components/footer.php
@@ -13,7 +13,7 @@ $displayFirstSection = $footerSiteTitleBlocks > 0 || $footerSocialBlocks > 0 || 
 <footer id="footer-theme">
     <div class="footer-wrapper ">
         <div class="footer-tab section-content-wrapper">
-            <a href="https://www.tradingcup.com" class="footer-logo">
+            <a href="<?php echo t('https://www.tradingcup.com/en');?>" class="footer-logo">
                 <img src="<?= $this->getThemePath() ?>/components/homepageSections/footer/logo.png" alt="">
             </a>
             <div class="footer-nav">
@@ -23,30 +23,30 @@ $displayFirstSection = $footerSiteTitleBlocks > 0 || $footerSocialBlocks > 0 || 
                 ?>
                 <ul>
                     <li>
-                        <a href="https://www.tradingcup.com">Home</a> |
+                        <a href="<?php echo t('https://www.tradingcup.com/en');?>"> <?php echo t("Home");?></a> |
                     </li>
 
                     <li>
-                        <a href="https://www.tradingcup.com/en/champions/2019-champions">2019 Champion</a> |
+                        <a href="<?php echo t('https://www.tradingcup.com/en');?>/champions/2019-champions"><?php echo t("2019 Champion");?></a> |
                     </li>
 
                     <li>
-                        <a href="https://www.tradingcup.com/en/champions/2018-champions">2018 Champion</a> |
+                        <a href="<?php echo t('https://www.tradingcup.com/en');?>/champions/2018-champions"><?php echo t("2018 Champion");?></a> |
                     </li>
 
                     <li>
-                        <a href="https://www.tradingcup.com/en/news">News</a> |
+                        <a href="<?php echo t('https://www.tradingcup.com/en');?>/news"><?php echo t("News");?></a> |
                     </li>
                     <li>
-                        <a href="https://www.tradingcup.com/en/faq">FAQ</a> |
-                    </li>
-
-                    <li>
-                        <a href="<?= $this->getThemePath() ?>/components/homepageSections/footer/2020-trading-cup-terms-and-conditions-english.pdf" target="_blank">T&Cs</a> |
+                        <a href="<?php echo t('https://www.tradingcup.com/en');?>/faq"><?php echo t("FAQ");?></a> |
                     </li>
 
                     <li>
-                        <a href="https://www.tradingcup.com/en/privacy">Privacy</a>
+                        <a href="<?= $this->getThemePath() ?>/components/homepageSections/footer/2020-trading-cup-terms-and-conditions-english.pdf" target="_blank"><?php echo t("T&Cs");?></a> |
+                    </li>
+
+                    <li>
+                        <a href="<?php echo t('https://www.tradingcup.com/en');?>/privacy"><?php echo t("Privacy");?></a>
                     </li> 
 
 
@@ -73,25 +73,8 @@ $displayFirstSection = $footerSiteTitleBlocks > 0 || $footerSocialBlocks > 0 || 
             </p>
             <p>
                 <?php echo t("disclaimer-line3");?>
-
             </p>
-
         </div>
     </div>
 </footer>
-
-<!--<footer id="concrete5-brand">-->
-<!--    <div class="container">-->
-<!--        <div class="row">-->
-<!--            <div class="col-sm-12">-->
-<!--                <span>--><?php //echo t('Built with <a href="http://www.concrete5.org" class="concrete5" rel="nofollow">concrete5</a> CMS.') ?><!--</span>-->
-<!--                <span class="pull-right">-->
-<!--                    --><?php //echo Core::make('helper/navigation')->getLogInOutLink() ?>
-<!--                </span>-->
-<!--                <span id="ccm-account-menu-container"></span>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</footer>-->
-
 <?php $this->inc('components/footer_bottom.php'); ?>
--- application/themes/customised/components/header.php
+++ application/themes/customised/components/header.php
@@ -4,25 +4,27 @@ $this->inc('components/header_top.php');
 
 <header>
     <div class="header-wrapper section-content-wrapper">
-            <a href="https://www.tradingcup.com"><img src="<?= $this->getThemePath() ?>/components/homepageSections/header/logo.png" alt=""></a>
+            <a href="<?php echo t('https://www.tradingcup.com/en');?>"><img src="<?= $this->getThemePath() ?>/components/homepageSections/header/logo.png" alt=""></a>
 
         <div class="header-right">
             <div class="header-nav">
                 <ul>
-                    <li><a href="https://www.tradingcup.com">Home</a></li>
-                                   <li><a href="https://www.tradingcup.com/en/news">News</a></li>
-                    <li id="dropdown-list-anchor">Past Champions <img
-                                src="<?= $this->getThemePath() ?>/components/homepageSections/header/Path.png"
-                                alt="arrow lost">
-                        <div id="campaign-dropdown-list">
-                            <a href="https://www.tradingcup.com/en/champions/2019-champions">2019 Champion</a>
-                            <a href="https://www.tradingcup.com/en/champions/2018-champions">2018 Champion</a>
+                    <li>
+                        <a href="<?php echo t('https://www.tradingcup.com/en');?>"><?php echo t("Home");?></a>
+                    </li>
+                                   <li><a href="<?php echo t('https://www.tradingcup.com/en');?>/news"><?php echo t("News");?></a></li>
+                                   <li id="dropdown-list-anchor"><?php echo t("Past Champions");?> <img
+                                   src="<?= $this->getThemePath() ?>/components/homepageSections/header/Path.png"
+                                   alt="arrow lost">
+                                   <div id="campaign-dropdown-list">
+                            <a href="<?php echo t('https://www.tradingcup.com/en');?>/champions/2019-champions"><?php echo t("2019 Champion");?></a>
+                            <a href="<?php echo t('https://www.tradingcup.com/en');?>/champions/2018-champions"><?php echo t("2018 Champion");?></a>
                         </div>
                     </li>
                 </ul>
 
             </div>
-            <button class="enter-button open-form">Enter 2020 Cup</button>
+            <button class="enter-button open-form"><?php echo t("Enter 2020 Cup");?></button>
             <?php
             $c= new Area('langauge-switch');
             $c->display();
@@ -39,14 +41,14 @@ $this->inc('components/header_top.php');
                  class="mobile-nav-btn">
             <div class="mobile-nav">
                 <ul>
-                    <li><a href="https://www.tradingcup.com">Home</a></li>
-                                       <li><a href="https://www.tradingcup.com/en/news">News</a></li>
-                    <li id="dropdown-list-anchor">Past Champions <img
-                                src="<?= $this->getThemePath() ?>/components/homepageSections/header/Path.png"
-                                alt="arrow lost">
+                    <li><a href="<?php echo t('https://www.tradingcup.com/en');?>"><?php echo t("Home");?></a></li>
+                                       <li><a href="<?php echo t('https://www.tradingcup.com/en');?>/news"><?php echo t("News");?></a></li>
+                    <li id="dropdown-list-anchor"><?php echo t("Past Champions");?> <img
+                    src="<?= $this->getThemePath() ?>/components/homepageSections/header/Path.png"
+                    alt="arrow lost">
                         <div id="campaign-dropdown-list">
-                            <a href="https://www.tradingcup.com/en/2019-champions/">2019 Champion</a>
-                            <a href="https://www.tradingcup.com/en/2018-champions/">2018 Champion</a>
+                            <a href="<?php echo t('https://www.tradingcup.com/en');?>/2019-champions/"><?php echo t("2019 Champion");?></a>
+                            <a href="<?php echo t('https://www.tradingcup.com/en');?>/2018-champions/"><?php echo t("2018 Champion");?></a>
                         </div>
                     </li>
                 </ul>
