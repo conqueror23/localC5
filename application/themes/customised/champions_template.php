94d37a682d6035206e780f18176bf22e567e405c
bruce.wang <bruce.wang@zerologix.com>
2020-06-17 12:03:37 +1000

    add champions template

---
+++ application/themes/customised/champions_template.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://lprsc.acycms.com/202005-tradingcup2018/dist/acysecurities/en/packages/antd-4.1.5/antd.min.css" rel="stylesheet">
+
+</head>
+<body>
+
+    <script type="text/javascript"
+        src="https://lprsc.acycms.com/202005-tradingcup2018/dist/acyid/en/packages/react-16.9.0/react.production.min.js"></script>
+    <script type="text/javascript"
+        src="https://lprsc.acycms.com/202005-tradingcup2018/dist/acyid/en/packages/react-dom-16.9.0/react-dom.production.min.js"></script>
+    <script type="text/javascript"
+        src="https://lprsc.acycms.com/202005-tradingcup2018/dist/acyid/en/packages/antd-4.1.5/antd-with-locales.min.js"></script>
+</body>
+</html>
