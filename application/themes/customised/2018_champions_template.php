d642f9c07bcdbe7272134b6eb14affa250828e09
bruce.wang <bruce.wang@zerologix.com>
2020-06-19 16:09:52 +1000

    syncrhonize with remote

---
+++ application/themes/customised/2018_champions_template.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link href="https://lprsc.acycms.com/202005-tradingcup2018/dist/acysecurities/en/packages/antd-4.1.5/antd.min.css" rel="stylesheet">
+    <style>
+        html body, body p, body span{
+            font-family:  "Helvetica Neue", "PingFang SC",  "Microsoft YaHei", "Tajawal", "Ayuthaya", Arial, sans-serif !important;
+        }
+    </style>
+</head>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<body style="background-color: #000;">
+    <div id='root'></div>
+    <script src="https://apiform.crm.zerologix.com/js/external/acyform.bundle.js" type="text/javascript" ></script>
+<script type="text/javascript" src="https://lprsc.acycms.com/202005-tradingcup2018/dist/acysecurities/en/packages/react-16.9.0/react.production.min.js"></script>
+<script type="text/javascript" src="https://lprsc.acycms.com/202005-tradingcup2018/dist/acysecurities/en/packages/react-dom-16.9.0/react-dom.production.min.js"></script>
+<script type="text/javascript" src="https://lprsc.acycms.com/202005-tradingcup2018/dist/acysecurities/en/packages/antd-4.1.5/antd-with-locales.min.js"></script>
+<script type="text/javascript" src="<?php echo t('https://lprsc.acycms.com/202005-tradingcup2018/dist/acysecurities/en/index6.js');?>"></script>
+
+<script>
+var sideMenu = new PromoLiveAccountSideMenu("Trading Cup Entry ", "Create a live trading account with ACY in a few simple steps. ", "en");
+  $(".open-form").on("click", function () {
+        sideMenu.openNav();
+        return false;
+    });
+</script>
+
+</body></html>
---
+++ application/themes/customised/2019_champions_template.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://lprsc.acycms.com/202005-tradingcup2019/dist/acyid/en/packages/antd-4.2.5/antd.min.css"
+        rel="stylesheet">
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <style>
+        html body, body p, body span{
+            font-family:  "Helvetica Neue", "PingFang SC",  "Microsoft YaHei", "Tajawal", "Ayuthaya", Arial, sans-serif !important;
+        }
+    </style>
+</head>
+<body>
+    <div id="root"></div>
+    <script src="https://apiform.crm.zerologix.com/js/external/acyform.bundle.js" type="text/javascript" ></script>
+    <script type="text/javascript"
+    src="https://lprsc.acycms.com/202005-tradingcup2019/dist/acyid/en/packages/react-16.13.1/react.production.min.js"></script>
+<script type="text/javascript"
+    src="https://lprsc.acycms.com/202005-tradingcup2019/dist/acyid/en/packages/react-dom-16.13.1/react-dom.production.min.js"></script>
+<script type="text/javascript"
+    src="https://lprsc.acycms.com/202005-tradingcup2019/dist/acyid/en/packages/antd-4.2.5/antd-with-locales.min.js"></script>
+<script type="text/javascript" src="https://lprsc.acycms.com/202005-tradingcup2019/dist/acyid/en/index7.js"></script>
+<script type="text/javascript" src="<?php echo t('https://lprsc.acycms.com/202005-tradingcup2019/dist/acysecurities/en/index7.js');?>"></script>
+
+
+<script>
+var sideMenu = new PromoLiveAccountSideMenu("Trading Cup Entry ", "Create a live trading account with ACY in a few simple steps. ", "en");
+  $(".open-form").on("click", function () {
+        sideMenu.openNav();
+        return false;
+    });
+</script>
+
+</body></html>
