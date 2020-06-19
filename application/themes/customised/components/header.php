3d1e9c509bd57edd8e118870e86d0c22f5bcf196
bruce.wang <bruce.wang@zerologix.com>
2020-06-19 13:41:00 +1000

    update mobile designs

--- application/themes/customised/components/header.php
+++ application/themes/customised/components/header.php
@@ -11,7 +11,9 @@ $this->inc('components/header_top.php');
                     <li>
                         <a href="<?php echo t('https://www.tradingcup.com/en');?>"><?php echo t("Home");?></a>
                     </li>
-                    <li><a href="<?php echo t('https://www.tradingcup.com/en');?>/news"><?php echo t("News");?></a></li>
+                    <li>
+                        <a href="<?php echo t('https://www.tradingcup.com/en');?>/news"><?php echo t("News");?></a>
+                    </li>
                     <li id="dropdown-list-anchor"><?php echo t("Past Champions");?>
                     <img src="<?= $this->getThemePath() ?>/components/homepageSections/header/Path.png" alt="arrow lost">
                         <div id="campaign-dropdown-list">
@@ -19,7 +21,7 @@ $this->inc('components/header_top.php');
                                 <?php echo t("2019 Champion");?>
                             </a>
                             <a href="<?php echo t('https://www.tradingcup.com/en');?>/champions/2018-champions">
-                            <?php echo t("2018 Champion");?>
+                                <?php echo t("2018 Champion");?>
                             </a>
                         </div>
                     </li>
@@ -27,33 +29,28 @@ $this->inc('components/header_top.php');
             </div>
 
             <button class="enter-button open-form"><?php echo t("Enter 2020 Cup");?></button>
-            <?php
-            $c= new Area('langauge-switch');
-            $c->display();
-            ?>
-            <select name="language" id="language-switch">
-                <option value="/en">
-                    English
-                </option>
-                <option value="/zh">
-                    Chinese
-                </option>
-            </select>
-            <img src="<?= $this->getThemePath() ?>/components/homepageSections/header/Shape.png" alt=""
-                 class="mobile-nav-btn">
-            <div class="mobile-nav">
-                <ul>
-                    <li><a href="<?php echo t('https://www.tradingcup.com/en');?>"><?php echo t("Home");?></a></li>
-                                       <li><a href="<?php echo t('https://www.tradingcup.com/en');?>/news"><?php echo t("News");?></a></li>
-                    <li id="dropdown-list-anchor"><?php echo t("Past Champions");?> <img
-                    src="<?= $this->getThemePath() ?>/components/homepageSections/header/Path.png"
-                    alt="arrow lost">
-                        <div id="campaign-dropdown-list">
-                            <a href="<?php echo t('https://www.tradingcup.com/en');?>/2019-champions/"><?php echo t("2019 Champion");?></a>
-                            <a href="<?php echo t('https://www.tradingcup.com/en');?>/2018-champions/"><?php echo t("2018 Champion");?></a>
-                        </div>
-                    </li>
-                </ul>
+            <div id='normal-language-switch'>
+                <?php
+                $c= new Area('langauge-switch');
+                $c->display();
+                ?>
+            </div>
+           
+            <img src="<?= $this->getThemePath() ?>/components/homepageSections/header/Hamburger.png" alt=""
+                 class="mobile-nav-btn" id='mobile-nav-btn'>
+            <div class="mobile-nav" id="mobile-nav-menu">
+                <div id="mobile-campaign-dropdown-list">
+                        <a href="<?php echo t('https://www.tradingcup.com/en');?>/champions/2019-champions/"><?php echo t("2019 Champion");?> ></a>
+
+                        <a href="<?php echo t('https://www.tradingcup.com/en');?>/champions/2018-champions/"><?php echo t("2018 Champion");?> ></a>
+                            <button id='mobile-enter-button' class="enter-button open-form"><?php echo t("Enter 2020 Cup");?></button>
+                            <div id='mobile-language-switch'>
+                            <?php
+                                $c= new Area('langauge-switch');
+                                $c->display();
+                                ?>
+                            </div>
+                </div>
             </div>
         </div>
 
--- application/themes/customised/components/homepageSections/header/header20200615.js
+++ application/themes/customised/components/homepageSections/header/header20200615.js
@@ -1,13 +1,55 @@
 "use strict";
 
+
 var toggleDropDown = function toggleDropDown() {
   document.getElementById("campaign-dropdown-list").classList.toggle("show");
 };
+var showTabletNavMenu =function showTabletNavMenu(mobileNav){
+  mobileNav.querySelector('#mobile-campaign-dropdown-list').setAttribute("style",'display:flex');
+}
 
-var listenAnchor = function listenAnchor() {
+var showPhoneNavMenu =function showPhoneNavMenu(mobileNav){
+  mobileNav.querySelector('ul').setAttribute('style','display:flex;flex-direction:column;align-items:center'); 
+
+}
+
+var showMobileNavMenu =function showMobileNavMenu(){
+  var windowWidth= window.innerWidth;
+  var mobileNav = document.getElementById('mobile-nav-menu');
+    mobileNav.setAttribute('style','opacity:1');
+    console.log(windowWidth);
+  if(windowWidth>378){
+    showTabletNavMenu(mobileNav);    
+  }else{
+    showPhoneNavMenu(mobileNav);
+  }
+}
+
+var transformMobileMenu= function transformMobileMenu() {
+  var navBtn= document.getElementById('mobile-nav-btn')
+  if(navBtn.getAttribute('src').includes('hamburger')) {
+    navBtn.setAttribute('src','/application/themes/customised/components/homepageSections/header/close.png')
+    navBtn.setAttribute('style','height:16px');
+    showMobileNavMenu();
+  }else{
+    navBtn.setAttribute('src','/application/themes/customised/components/homepageSections/header/hamburger.png')
+    navBtn.setAttribute('style','height:10px');
+    document.getElementById('mobile-nav-menu').setAttribute('style','opacity:0');
+  }
+}
+
+var toggleMobileMenu = function toggleMobileMenu(){
+  console.log('opening mobile nav menus');
+  transformMobileMenu()
+}
+
+var listenChampionAnchor = function listenChampionAnchor() {
   document.getElementById("dropdown-list-anchor").addEventListener("click",toggleDropDown);
 };
 
+ (function (){
+   return document.getElementById('mobile-nav-btn').addEventListener("click",toggleMobileMenu);
+})()
 // var sideMenu = new PromoLiveAccountSideMenu('Trading Cup Entry', "Create a live trading account with ACY in a few simple steps.", "en");
 var mobileBtn = $(".mobile-nav-btn");
 
@@ -16,5 +58,4 @@ mobileBtn.on("click", function () {
   mobileNav.fadeIn(300);
 });
 
-
-listenAnchor();
+listenChampionAnchor();
