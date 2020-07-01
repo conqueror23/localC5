d80335318907b7273094b3b30b3272d84061b951
bruce.wang <bruce.wang@zerologix.com>
2020-07-01 11:54:44 +1000

    update project structures and change source files

--- application/themes/customised/components/homepageSections/socialMedia/socialMedia.php
+++ application/themes/customised/components/homepageSections/socialMedia/socialMedia.php
@@ -1,12 +1,12 @@
 <div class="social-wrapper">
             <a href="https://twitter.com/trading_cup" target="_blank">
-               <img src="<?php echo $view->getThemePath() ?>/components/homepageSections/socialMedia/Twiiter.png" alt="">
+               <img src="<?php echo $view->getThemePath() ?>/components/homepageSections/socialMedia/twiter-logo.png" alt="">
             </a>
             <a href="https://www.facebook.com/thetradingcup" target="_blank">
-                <img src="<?php echo $view->getThemePath() ?>/components/homepageSections/socialMedia/Vector.png" alt="">
+                <img src="<?php echo $view->getThemePath() ?>/components/homepageSections/socialMedia/facebook-logo.png" alt="">
             </a>
             <a href="https://www.youtube.com/channel/UCumvntEVJuLD6INzqI9yDIg/featured" target="_blank">
-                <img src="<?php echo $view->getThemePath() ?>/components/homepageSections/socialMedia/youtube.png" alt="">
+                <img src="<?php echo $view->getThemePath() ?>/components/homepageSections/socialMedia/youtube-logo.png" alt="">
             </a>
     </div>
     
