0f29ba950452463e178f4255089aa70d0e7cb97b
bruce.wang <bruce.wang@zerologix.com>
2020-07-01 16:10:59 +1000

    update 2019 2018 champions

--- application/themes/customised/2019_champions_template.php
+++ application/themes/customised/2019_champions_template.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://lprsc.acycms.com/202005-tradingcup2019/dist/acyid/en/packages/antd-4.2.5/antd.min.css"
+    <link href="https://lprsc.acycms.com/202005-tradingcup2019/dist/acysecurities/en/packages/antd-4.2.5/antd.min.css"
         rel="stylesheet">
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <style>
@@ -17,11 +17,11 @@
     <div id="root"></div>
     <script src="https://apiform.crm.zerologix.com/js/external/acyform.bundle.js" type="text/javascript" ></script>
     <script type="text/javascript"
-    src="https://lprsc.acycms.com/202005-tradingcup2019/dist/acyid/en/packages/react-16.13.1/react.production.min.js"></script>
+    src="https://lprsc.acycms.com/202005-tradingcup2019/dist/acysecurities/en/packages/react-16.13.1/react.production.min.js"></script>
 <script type="text/javascript"
-    src="https://lprsc.acycms.com/202005-tradingcup2019/dist/acyid/en/packages/react-dom-16.13.1/react-dom.production.min.js"></script>
+    src="https://lprsc.acycms.com/202005-tradingcup2019/dist/acysecurities/en/packages/react-dom-16.13.1/react-dom.production.min.js"></script>
 <script type="text/javascript"
-    src="https://lprsc.acycms.com/202005-tradingcup2019/dist/acyid/en/packages/antd-4.2.5/antd-with-locales.min.js"></script>
+    src="https://lprsc.acycms.com/202005-tradingcup2019/dist/acysecurities/en/packages/antd-4.2.5/antd-with-locales.min.js"></script>
 <script type="text/javascript" src="<?php echo t('https://lprsc.acycms.com/202005-tradingcup2019/dist/acysecurities/en/index7.js');?>"></script>
 
 <script>
