ae795b7a11488fe22b4962b0acb19df913da62f0
bruce.wang <bruce.wang@zerologix.com>
2020-08-14 17:47:18 +1000

    update footer set ups

--- application/themes/customised/components/footer_bottom.php
+++ application/themes/customised/components/footer_bottom.php
@@ -2,11 +2,11 @@
 
 </div>
 <script src="<?php echo $view->getThemePath();?>/components/homepageSections/highlight2019/assets/index.js"></script>
-<script src="<?php echo $view->getThemePath() ?>/js/globalActions.js"></script>
-<script src="<?= $this->getThemePath()?>/components/homepageSections/subscribeForm/getCountryList-9c3e5.js"></script>
+<script src="<?php echo $view->getThemePath() ?>/js/globalActions.js" defer></script>
+<script src="<?= $this->getThemePath()?>/components/homepageSections/subscribeForm/getCountryList-9c3e5.js" defer></script>
 <?php if(Localization::activeLanguage() ==='en'){ ?>
-<script src="<?= $this->getThemePath() ?>/components/homepageSections/subscribeForm/submitSub.js"></script>
-<script src="<?= $this->getThemePath() ?>/components/homepageSections/subscribeForm/getCountryDropDownList-9c3e5.js"></script>
+<script src="<?= $this->getThemePath() ?>/components/homepageSections/subscribeForm/submitSub.js" defer></script>
+<script src="<?= $this->getThemePath() ?>/components/homepageSections/subscribeForm/getCountryDropDownList-9c3e5.js" defer></script>
 <?php }?>
 
 <!--<script src="--><?//= $this->getThemePath() ?><!--/components/homepageSections/highlight2019/assets/index.js"></script>-->
